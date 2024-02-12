f68c3b94dd104d306e81f17d4a200ae23413b224
Parth <parth.sce21@sot.pdpu.ac.in>
2024-02-12 20:30:15 +0530

    3Sum

--- LeetCode/Medium/15.cpp
+++ LeetCode/Medium/15.cpp
@@ -2,40 +2,73 @@
 using namespace std;
 // 3sum
 
-vector<vector<int>> threeSumm(vector<int> &nums){
-    int tsum;
-    vector<int> res ;
-    vector<vector<int>> final ;
-    for(int i = 0;i<nums.size();i++){  
-        if(res.size()<3){
-            res.push_back(nums[i]);
-            tsum+=nums[i];
-        }
-        if(res.size()==3){
-            if(tsum==0){
-                final.push_back(res);
+
+vector<vector<int>> t3eeSum(vector<int> &nums){
+    vector<vector<int>> ans;
+    int sum;
+    sort(nums.begin(), nums.end());
+    for(int i=0;i<nums.size();i++){
+        if(i!=0 && nums[i]==nums[i-1]) continue;
+        int j = i+1;
+        int k = nums.size() -1;
+        while(j<k){
+            int sum = nums[i] + nums[j]+nums[k];
+            if(sum<0){
+                j++;
             }
-            tsum-=res[0];
-            res.erase(res.begin());
+            else if(sum>0){
+                k--;
+            }
+            else {
+                vector<int> temp ={nums[i],nums[j],nums[k]};
+                ans.push_back(temp);
+                j++;k--;
+                while(j<k && nums[j] == nums[j-1]) j++;
+                while(j<k && nums[k]== nums[k-1]) k--;
+            }
         }
     }
-    return final;
+    return ans;
 }
+
+
+
+
+
+// vector<vector<int>> threeSumm1(vector<int> &nums){
+//     int tsum;
+//     vector<int> res ;
+//     vector<vector<int>> final ;
+//     for(int i = 0;i<nums.size();i++){  
+//         if(res.size()<3){
+//             res.push_back(nums[i]);
+//             tsum+=nums[i];
+//         }
+//         if(res.size()==3){
+//             if(tsum==0){
+//                 final.push_back(res);
+//             }
+//             tsum-=res[0];
+//             res.erase(res.begin());
+//         }
+//     }
+//     return final;
+// }
 void printArr(vector<vector<int>> &mn){
     for(auto i:mn){
         for(auto j:i){
             cout<<j<<" ";
     }
-    cout<<" : "<<endl;
+    cout<<" : ";
     }
 }
 
 int main(){
     vector<int> jg = {-1,0,1,2,-1,-4};
-    // we need to sort the given aray
+     
     sort(jg.begin(),jg.end()) ;
-    // now call the function and store in variable  
-    vector<vector<int>> mn = threeSumm(jg) ;
+   
+    vector<vector<int>> mn = t3eeSum(jg) ;
     printArr(mn);
     
 }
