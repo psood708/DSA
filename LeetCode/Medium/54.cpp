dec92e3ebf5f84c6f7bb489f47f04b64198a9928
Parth <parth.sce21@sot.pdpu.ac.in>
2024-02-12 06:40:20 +0530

    Spiral order

---
+++ LeetCode/Medium/54.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int top = 0, bottom = n - 1;
+    int left = 0,right = m - 1;
+    vector<int> ans ;
+    while(top<=bottom && left<=right){
+        for(int i = left;i <= right ;i++){
+            ans.push_back(matrix[top][i]);
+        }
+        top++;
+        for(int i =top;i<=bottom;i++){
+            ans.push_back(matrix[i][right]);
+        }
+        right--;
+        if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        if(left<=right){
+            for(int i=bottom;i>=top;i--){
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<vector<int>> mat = {{1,2,3},{4,5,6},{7,8,9}};
+    vector<int> res = spiralOrder(mat);
+    for(auto &x:res) cout<<x<<" ";
+    return 0;
+}
+
+ 
